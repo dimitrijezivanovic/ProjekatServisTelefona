15bd1bd84fafc839420db9921987117ab4049a3b
Luka Raicevic <raicevic12345@gmail.com>
2020-05-27 20:47:11 +0200

    PrviKomit

---
+++ src/test/java/GrupaC/demo/ServisTelefonaApplicationTests.java
@@ -0,0 +1,13 @@
+package GrupaC.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisTelefonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
